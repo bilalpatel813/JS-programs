f226021614a413535c77032c1357bf8cac8d4c2c
Admin <bilalpatel78600@gmail.com>
2026-06-29 13:40:38 +0530

    scope&closures

---
+++ scope&closures.js
@@ -0,0 +1,17 @@
+//scope 
+let globalvar = "global var";
+function outer(){
+    let outervar =  "inside outer func";
+    function inner(){
+        let innervar = "inside inner func";
+        console.log(globalvar);
+        console.log(outervar);
+        console.log(innervar);
+    }
+    inner();
+}
+outer();
+//closures
+function MakeCounter(){
+    
+}
